b12b1957d8e839a59542b0fcdcca076bd8ce570b
Kyle <kjbowman00@vt.edu>
2020-04-18 14:01:41 -0400

    Debug Menu added. Open with LCtrl + g

---
+++ LudumDare46/Assets/Scripts/DebugTools.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebugTools : MonoBehaviour
+{
+    private Canvas cv;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cv = GetComponent<Canvas>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftControl) && Input.GetKeyDown(KeyCode.G))
+        {
+            cv.enabled = !cv.enabled;
+        }
+    }
+}
