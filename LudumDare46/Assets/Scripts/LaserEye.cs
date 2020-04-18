187bbe2fbc9306bed8d192af456517a571f40abd
iluvwaffles2 <iluvwaffles4@gmail.com>
2020-04-18 19:58:43 -0400

    Started work on line laser object

---
+++ LudumDare46/Assets/Scripts/LaserEye.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserEye : MonoBehaviour
+{
+	
+	private LineRenderer render;
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+        render = GetComponent<LineRenderer>();
+		render.SetPosition(0, new Vector3(0f, 0f, 0f));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		render.SetPosition(1, new Vector3(1f, -1f, 0f)); 
+    }
+}
