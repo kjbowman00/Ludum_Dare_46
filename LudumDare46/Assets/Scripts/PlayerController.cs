5761e1b315ff7db63be6dc2482db056da2d12076
Kyle <kjbowman00@vt.edu>
2020-04-18 02:47:07 -0400

    i hate unity physics

---
+++ LudumDare46/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    public float jumpForce;
+
+    private Rigidbody2D rb;
+
+
+    private bool a, d;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xVel = Input.GetAxis("Horizontal") * speed;
+        rb.velocity = new Vector2(xVel, rb.velocity.y);
+        //transform.Translate(Vector3.right * Time.deltaTime * speed * Input.GetAxis("Horizontal"));
+    }
+}
