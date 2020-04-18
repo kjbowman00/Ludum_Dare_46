3328161d6e0d176e5d41e5f14ab39a8274c923d5
Kyle <kjbowman00@vt.edu>
2020-04-18 17:54:50 -0400

    Interactable class and pump

---
+++ LudumDare46/Assets/Scripts/Pump.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pump : Interactable
+{
+
+    public float pumpDuration;
+    public float randomness;
+    public int pumpFixNeeded;
+
+    private int pumpFixPassed;
+
+    private bool pumpOn;
+    private float timePassed;
+    private float timeNeeded;
+    // Start is called before the first frame update
+    void Start()
+    {
+        pumpOn = true;
+        timePassed = 0;
+        timeNeeded = pumpDuration + Random.Range(0, randomness);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (pumpOn)
+        {
+            if (timePassed < timeNeeded)
+            {
+                timePassed += Time.deltaTime;
+            }
+            else
+            {
+                timePassed = 0;
+                timeNeeded = pumpDuration + Random.Range(0, randomness);
+                pumpOn = false;
+                GetComponent<SpriteRenderer>().color = Color.red; //Temporary until we animate
+            }
+        } 
+        else
+        {
+            if (pumpFixPassed >= pumpFixNeeded)
+            {
+                //Pump is now fixed
+                pumpOn = true;
+                pumpFixPassed = 0;
+                GetComponent<SpriteRenderer>().color = Color.white; //Temporary until we animate
+            }
+        }
+    }
+
+    public override int interact()
+    {
+        if (!pumpOn)
+        {
+            pumpFixPassed++;
+        }
+
+        return 0;
+    }
+}
