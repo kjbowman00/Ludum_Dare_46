e3f2c65888c398a07ab96c27a2cc171636c1c1c1
Kyle <kjbowman00@vt.edu>
2020-04-19 16:10:05 -0400

    Player animated

---
+++ LudumDare46/Assets/Scripts/Player/PlayerAnimator.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimator : MonoBehaviour
+{
+    Animator animator;
+    SpriteRenderer sr;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        sr = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float axis = Input.GetAxis("Horizontal");
+        if (axis == 0)
+        {
+            animator.SetBool("Moving", false);
+        }
+        else if (axis > 0)
+        {
+            animator.SetBool("Moving", true);
+            sr.flipX = false;
+        }
+        else if (axis < 0)
+        {
+            animator.SetBool("Moving", true);
+            sr.flipX = true;
+        }
+    }
+}
