30b561d50568ea210f10dfd5d812e4df0d0d8c23
Kyle <kjbowman00@vt.edu>
2020-04-19 17:19:12 -0400

    Baby breathing animation

---
+++ LudumDare46/Assets/Scripts/Baby/BabyArmsUpDown.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BabyArmsUpDown : MonoBehaviour
+{
+    public float shiftAmount;
+    public float speed;
+    private float halfShift;
+    private float startY;
+    // Start is called before the first frame update
+    void Start()
+    {
+        startY = transform.position.y;
+        halfShift = shiftAmount / 2;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float ping = Mathf.PingPong(Time.time*speed, shiftAmount);
+        transform.position = new Vector3(transform.position.x, ping - halfShift + startY, transform.position.z);
+    }
+}
