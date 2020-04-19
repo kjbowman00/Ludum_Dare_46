38b2d4bc184cac8522963890fcedc5ae9aa3235c
iluvwaffles2 <iluvwaffles4@gmail.com>
2020-04-19 01:44:20 -0400

    Added in milk spawning to the babby

---
+++ LudumDare46/Assets/Scripts/BigBaby.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BigBaby : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+	
+	void spawnMilk(float x, float y)
+	{
+		var milk = Resources.Load<Object>("Milk Spill") as GameObject;
+		var createdMilk = Object.Instantiate(milk, new Vector3(x, y, 0), Quaternion.identity);
+		Debug.Log("Created " + createdMilk.name + " at " + x + ", " + y + ".");
+	}
+}
