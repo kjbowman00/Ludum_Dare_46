b574c298ebb7729869c70aac9ae494c004d11baf
Kyle <kjbowman00@vt.edu>
2020-04-20 18:47:38 -0400

    Baby turns red now

--- LudumDare46/Assets/Scripts/GameEndManager.cs
+++ LudumDare46/Assets/Scripts/GameEndManager.cs
@@ -4,9 +4,12 @@ using UnityEngine;
 
 public class GameEndManager : MonoBehaviour
 {
+    public SpriteRenderer[] babySprites;
     public GameObject clockHand;
     public float TOTAL_TIME_NEEDED;
     private float timePassed;
+
+    private float updateVisualsClock = 0;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,9 +19,20 @@ public class GameEndManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        updateVisualsClock += Time.deltaTime;
         timePassed += Time.deltaTime;
-        float percentage = timePassed / TOTAL_TIME_NEEDED;
-        clockHand.transform.localRotation = Quaternion.Euler(0, 0, percentage * -360);
+        if (updateVisualsClock > 3f)
+        {
+            float percentage = timePassed / TOTAL_TIME_NEEDED;
+            clockHand.transform.localRotation = Quaternion.Euler(0, 0, percentage * -360);
+            float invertedP = 1 - percentage;
+            updateVisualsClock = 0;
+            for (int i = 0; i < babySprites.Length; i++)
+            {
+                babySprites[i].color = new Color(1, Mathf.Lerp(0.5f, 1, invertedP), Mathf.Lerp(0.5f, 1, invertedP));
+            }
+        }
+      
         if (timePassed > TOTAL_TIME_NEEDED)
         {
             //WIN!!!
