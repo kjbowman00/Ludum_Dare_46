cd1f7ada4ae01edb54f385a6a673e6f1c8288eb7
Kyle <kjbowman00@vt.edu>
2020-04-20 17:38:16 -0400

    Win Scene Done

---
+++ LudumDare46/Assets/BossWin.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossWin : MonoBehaviour
+{
+    private AudioSource myAudio;
+    float timer = 0;
+    float pitchTime = 9f;
+    float endTime = 16f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myAudio = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer >= pitchTime) myAudio.pitch = 0.7f;
+        if (timer >= endTime)
+        {
+            myAudio.pitch = 1f;
+            SceneChanger.instance.toMenu();
+        }
+    }
+}
